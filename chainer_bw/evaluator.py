9d7310e6b760e6b72cbd96e9a7f088ab0bba8599
teffland <tom.effland@gmail.com>
2017-06-06 10:34:01 -0400

    "auto commit tracked files for new experiment: experiments/Jun-06-2017-10.34.332208_experiment"

--- chainer_bw/evaluator.py
+++ chainer_bw/evaluator.py
@@ -2,16 +2,16 @@ import copy
 import chainer as ch
 
 class VariableConverterEvaluator(ch.training.extensions.Evaluator):
-    """ An implementation of an evaluator that doesn't assume 
+    """ An implementation of an evaluator that doesn't assume
     the downstream model will receive a tuple or dict of chainer variables.
-    
+
     Instead, this evaluator let's the converter function produce an arbitrary
     data structure of :class:`ch.Variable`s for consumption by downstream models.
-    
+
     It is the sibling implementation of :class:`VariableConverterUpdater`.
-    
+
     The `converter` function must take the keyword argument `volatile`.
-    
+
     This is useful for sequence-based models, where often we want to pass
     entire lists of :class:`ch.Variable`s as arguments.
     """
@@ -41,13 +41,14 @@ class VariableConverterEvaluator(ch.training.extensions.Evaluator):
         for batch in it:
             observation = {}
             with ch.reporter.report_scope(observation):
-                in_vars = self.converter(batch, volatile='on')
-                if type(in_vars) in (tuple, list):
-                    eval_func(*in_vars)
-                elif isinstance(in_vars, dict):
-                    eval_func(**in_vars)
-                else:
-                    eval_func(in_vars)
+                with ch.no_backprop_mode():
+                    in_vars = self.converter(batch)
+                    if type(in_vars) in (tuple, list):
+                        eval_func(*in_vars)
+                    elif isinstance(in_vars, dict):
+                        eval_func(**in_vars)
+                    else:
+                        eval_func(in_vars)
             summary.add(observation)
 
         return summary.compute_mean()
--- nli_converter.py
+++ nli_converter.py
@@ -4,9 +4,9 @@ import chainer as ch
 class NLIBatchConverter(object):
     """ Converter for use with :class:`VariableConverterUpdater` and
     :class:`VariableConverterEvaluator`.
-    
+
     This object converts string tokens to ids for each sequence,
-    then wraps that sequence in a ch.Variable, passing back a full 
+    then wraps that sequence in a ch.Variable, passing back a full
     dictionary of variables.
     """
     def __init__(self, token_vocab, class_vocab):
@@ -17,31 +17,32 @@ class NLIBatchConverter(object):
         """
         self.token_vocab = token_vocab
         self.class_vocab = class_vocab
-        
-    def __call__(self, batch, volatile='off'):
-        """ Convert a batch of data into dictionary containing input data 
+
+    def __call__(self, batch):
+        """ Convert a batch of data into dictionary containing input data
         for downstream NLI model.
-        
+
         Args:
             batch (list): list of tokenized NLI data
-            
+
         Returns:
             in_vars (dict): dictionary of converted NLI data
               'hs': list of variable arrays of hypothesis sentences
               'ps': list of variable arrays of premise sentences
               'cs': variable array of classes
         """
+        with chainer.using_config
         hs = [ ch.Variable(np.array(
-                 [ self.token_vocab.idx(token) for token in datum['h'] ], 
-               dtype=np.int32), volatile=volatile)
+                 [ self.token_vocab.idx(token) for token in datum['h'] ],
+               dtype=np.int32))
                for datum in batch ]
         ps = [ ch.Variable(np.array(
-                 [ self.token_vocab.idx(token) for token in datum['p'] ], 
-               dtype=np.int32), volatile=volatile)
+                 [ self.token_vocab.idx(token) for token in datum['p'] ],
+               dtype=np.int32))
                for datum in batch ]
         cs = ch.Variable(np.array(
                [ self.class_vocab.idx(datum['c']) for datum in batch ],
-             dtype=np.int32), volatile=volatile)
+             dtype=np.int32))
         in_vars = {
             'hs':hs,
             'ps':ps,
